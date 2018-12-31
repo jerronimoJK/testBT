367935e3a6ebf19f08253eed47ddd990b5652885
jerry <jerry@jerry.com>
2018-12-31 17:59:56 +0100

    Updates

---
+++ precache-manifest.cab3f01b22ead0c9e41ce4d109b1f7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testBT/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5b42be328bcdbcbbd7f",
+    "url": "/testBT/static/js/runtime~main.f5b42be3.js"
+  },
+  {
+    "revision": "4beecda433823a9c2a74",
+    "url": "/testBT/static/js/main.4beecda4.chunk.js"
+  },
+  {
+    "revision": "36d01552d46e48f8115a",
+    "url": "/testBT/static/js/1.36d01552.chunk.js"
+  },
+  {
+    "revision": "4beecda433823a9c2a74",
+    "url": "/testBT/static/css/main.cc42365d.chunk.css"
+  },
+  {
+    "revision": "41bcf950032791696d6f2944b591b8a8",
+    "url": "/testBT/index.html"
+  }
+];
